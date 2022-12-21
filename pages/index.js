44facececfa385d6d7cf37fe9f92112711ec7e76
abdulquadir77 <abdulquadir77@yahoo.com>
2022-12-21 22:49:54 +0530

    Header Added

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { Inter } from "@next/font/google";
 import { Box } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
 import Pricing from "../components/Pricing";
+import Header from "../components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function Home() {
       </Head>
       <Box>
         <Navbar />
+        <Header />
         <Pricing />
       </Box>
     </>
