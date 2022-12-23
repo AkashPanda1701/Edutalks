62b5ee9fed8f115e978a2a9716c7c8828752b0f7
Chandan <indspunk@gmail.com>
2022-12-23 13:10:31 +0530

    Chat Created

---
+++ pages/api/socket.js
@@ -0,0 +1,21 @@
+import { Server } from 'Socket.IO'
+
+const SocketHandler = (req, res) => {
+  if (!res.socket.server.io) {
+    console.log('Socket.IO starting')
+    const io = new Server(res.socket.server)
+    io.on('connection', (socket) => {
+      console.log('Socket.IO connected')
+      socket.on('disconnect', () => {
+        console.log('Socket.IO disconnected')
+      })
+      socket.on('message', (data) => {
+        console.log(data)
+      } )
+    })
+    res.socket.server.io = io
+  }
+  res.end()
+}
+
+export default SocketHandler
