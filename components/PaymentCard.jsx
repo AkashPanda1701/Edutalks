f473145efa2488519c4229c7f5a5114f58a2a358
abdulquadir77 <abdulquadir77@yahoo.com>
2022-12-23 14:16:04 +0530

    payment card added

--- components/PaymentCard.jsx
+++ components/PaymentCard.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import {
   Box,
   Button,
@@ -13,13 +14,47 @@ import {
   ModalOverlay,
   ModalHeader,
   ModalCloseButton,
+  ChakraBaseProvider,
+  chakra,
+  Text,
+  Flex,
+  useToast,
 } from "@chakra-ui/react";
 
 const PaymentCard = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const finalRef = React.useRef(null);
-
   const initialRef = React.useRef(null);
+  const toast = useToast();
+  const [cvvData, setCvvData] = useState();
+
+  const handleChange = (event) => {
+    setCvvData(event.target.value);
+  };
+  //console.log(cvvData);
+  const handleSubmit = () => {
+    if (cvvData === "123") {
+      toast({
+        title: "Payment Successful",
+        // description: "We've created your account for you.",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+        position: "top",
+      });
+      onClose();
+    } else {
+      toast({
+        title: "CVV Wrong",
+        // description: "We've created your account for you.",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+        position: "top",
+      });
+    }
+  };
+
   return (
     <>
       <Button
@@ -60,19 +95,46 @@ const PaymentCard = () => {
           <ModalHeader>Pay using card</ModalHeader>
           <ModalCloseButton />
           <ModalBody pb={6}>
-            <FormControl>
-              <FormLabel>First name</FormLabel>
-              <Input ref={initialRef} placeholder="First name" />
-            </FormControl>
-
-            <FormControl mt={4}>
-              <FormLabel>Last name</FormLabel>
-              <Input placeholder="Last name" />
-            </FormControl>
+            <Box
+              w="90%"
+              m="auto"
+              bg="#022c4f"
+              h="200px"
+              color="white"
+              p="15px"
+              borderRadius="8px"
+            >
+              <chakra.h1>HDFC CREDIT CARD</chakra.h1>
+              <Text
+                textAlign="center"
+                letterSpacing="5px"
+                mt="20px"
+                fontSize="23px"
+              >
+                1234 4567 1234 4567
+              </Text>
+              <Flex justifyContent="space-between" align="right">
+                <Box>
+                  <Text mt="18px">Expiry</Text>
+                  <Text>01/23</Text>
+                </Box>
+                <Box>
+                  <FormControl w="30%">
+                    <FormLabel>CVV</FormLabel>
+                    <Input
+                      placeholder="CVV"
+                      required
+                      maxLength="3"
+                      onChange={handleChange}
+                    />
+                  </FormControl>
+                </Box>
+              </Flex>
+            </Box>
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="blue" mr={3}>
+            <Button colorScheme="blue" mr={3} onClick={handleSubmit}>
               Pay
             </Button>
             {/* <Button onClick={onClose}>Cancel</Button> */}
